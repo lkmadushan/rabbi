3443ccd1b1dda21a856e0ea09b7497f281a89a1d
Kalpa Perera <kalpa.lkm@gmail.com>
2023-11-20 10:54:30 +0530

    Refactoring

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Quote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Session;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap'
 import { createApp } from 'vue'
-import App from './pages/App.vue'
+import App from './components/App.vue'
 import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 import router from './routes/index.js'
 

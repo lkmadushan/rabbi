f177f06e62250f28093f16290db63f72ea344a1b
Kalpa Perera <kalpa.lkm@gmail.com>
2024-04-01 10:57:52 +0530

    Changed instructions

--- resources/js/pages/Instructions.vue
+++ resources/js/pages/Instructions.vue
@@ -18,7 +18,7 @@
             <div class="space-x-1.5 flex">
                 <div class="text-primary whitespace-nowrap">Step 1:</div>
                 <div class="flex flex-1">
-                    <div class="mr-2">Click ‘share button’</div>
+                    <div class="mr-2">At the bottom of the page, click ‘share button’</div>
                     <button type="button" class="pointer-events-none w-8 h-8 -mt-0.5 group flex items-center justify-center bg-primary text-white hover:bg-white hover:text-primary focus:outline-0 focus:ring-2 focus:ring-white rounded-full transition delay-100">
                         <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M0 0h24v24H0V0z" fill="none"/><path fill="currentColor" d="M16 5l-1.42 1.42-1.59-1.59V16h-1.98V4.83L9.42 6.42 8 5l4-4 4 4zm4 5v11c0 1.1-.9 2-2 2H6c-1.11 0-2-.9-2-2V10c0-1.11.89-2 2-2h3v2H6v11h12V10h-3V8h3c1.1 0 2 .89 2 2z"/>
